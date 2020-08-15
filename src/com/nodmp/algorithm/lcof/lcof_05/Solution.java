5d88d686129247592ebd16dc5fe722d79d50ca58
qinglin <871001110@qq.com>
2020-08-15 23:21:23 +0800

    更新剑指Offer 05(100%)

---
+++ src/com/nodmp/algorithm/lcof/lcof_05/Solution.java
@@ -0,0 +1,10 @@
+package com.nodmp.algorithm.lcof.lcof_05;
+
+/**
+ * https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+ */
+public class Solution {
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
