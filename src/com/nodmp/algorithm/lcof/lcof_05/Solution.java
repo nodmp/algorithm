a03119ee9866be9c9a2b782a95d7dd2adda4b703
qinglin <871001110@qq.com>
2020-08-15 23:26:42 +0800

    更新剑指Offer 05(100%) / 2

--- src/com/nodmp/algorithm/lcof/lcof_05/Solution.java
+++ src/com/nodmp/algorithm/lcof/lcof_05/Solution.java
@@ -7,4 +7,21 @@ public class Solution {
     public String replaceSpace(String s) {
         return s.replace(" ", "%20");
     }
+
+    public String replaceSpace2(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                stringBuilder.append("%20");
+            } else {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = new Solution().replaceSpace("We are happy.");
+
+    }
 }
